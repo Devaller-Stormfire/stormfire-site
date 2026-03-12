4a11a4d60aecdb72d47f97cefbee647e4715001d
Devaller-Stormfire <guevendenis@icloud.com>
2026-03-12 05:08:34 +0100

    site.js aktualisieren

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,9 @@
 (function () {
   try {
     const path = location.pathname.replace(/\/+$/, "") || "/index.html";
-
     document.querySelectorAll(".navbtn").forEach((a) => {
       const href = (a.getAttribute("href") || "").replace(/\/+$/, "");
-      if (href === path) {
-        a.classList.add("active");
-      }
+      if (href === path) a.classList.add("active");
     });
   } catch (err) {
     console.error("[site.js]", err);
